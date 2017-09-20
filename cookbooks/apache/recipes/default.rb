0c70375cae94e8c71627baa5cfbe566c592ef26d
stephen <stephen@kalapati.com>
2017-09-20 12:33:31 -0600

    added motd and conditionals to apache

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -4,6 +4,12 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 #
+if node['platform_family'] == 'rhel'
+	package = 'httpd'
+elsif node['platform_family'] == 'debian'
+	package = 'apache2'
+end
+
 package 'apache2' do
 	package_name 'httpd'
 	action :install
@@ -13,4 +19,3 @@ service 'apache2' do
 	service_name 'httpd'
 	action [:enable, :start]
 end
-
